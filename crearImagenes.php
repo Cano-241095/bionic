225c2138f9d1fac1c15bf9443d0c93c225f4fd8c
diana <dianalaugr13@gmail.com>
2021-11-13 10:23:25 -0600

    Ajuste de vista en Imagenes

--- crearImagenes.php
+++ crearImagenes.php
@@ -1,12 +1,12 @@
 <?php
     include ("conexion.php");
-    if (isset($_POST['enviar'])){
-        $id = $_POST['id'];
+    if (isset($_POST['enviar2'])){
+        $url = $_FILES["image"]["name"];
         $id_asociado = $_POST['id_asociado'];
-        $imagen= $_POST['imagen'];
-
+        $id_imagen = $_POST['id'];
+               
         $insert = "INSERT INTO imagenes (id,id_asociado,imagen)
-        VALUES ('$id','$id_asociado','$imagen')";
+        VALUES ('$id','$id_asociado','$url')";
     
         if (mysqli_query($conn,$insert)){
             $_SESSION['message'] = 'Registro guardado exitosamente';
@@ -16,4 +16,54 @@
         echo "El registro no se pudo guardar". mysqli_error($conn);
         }        
     }
+
+    $url = $_FILES["image"]["name"];
+$target_dir = "img/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if(isset($_POST["enviar"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["image"]["size"] > 700000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
 ?>
