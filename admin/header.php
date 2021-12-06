aaba9551b8031a595c0c37adf10cdcdb5760bb60
diana <dianalaugr13@gmail.com>
2021-12-06 13:18:46 -0600

    presupuesto y ventas

--- admin/header.php
+++ admin/header.php
@@ -1,9 +1,9 @@
 <header class="sticky-top">
     <a href="home.php"><img src="../img/completo1.png" alt="logoEntrada"></a>
     <nav>
-        <a href="#">COMPAÑIA</a>
+        <a href="presupuesto.php">PRESUPUESTOS</a>
         <a href="productos.php">PRODUCTOS</a>
-        <a href="#">TIENDA</a>
+        <a href="venta.php">VENTAS</a>
         <a href="#">CONTACTO</a>
         <a href="#"><i class="bi bi-search"></i></a>
         <a href="#"><i class="bi bi-cart"></i></a>
